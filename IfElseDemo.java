c0eb4d3c8b39c9e15145aa11370577281ad90f70
Sneha2120 <snehagowdas473@gmail.com>
2024-11-25 14:18:45 +0530

    Add files via upload

---
+++ IfElseDemo.java
@@ -0,0 +1,20 @@
+package com.anpc8323.day2;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		int number1 = 20;
+		int number2 = 10;
+		
+		if(number1 <= number2)
+		{
+			System.out.println(number1 + " is lesser than or equal to " + number2);
+		}
+		else
+		{
+			System.out.println(number1 + " is greater than " + number2);
+		}
+	}
+
+}
---
+++ SimpleIfDemo.java
@@ -0,0 +1,20 @@
+package com.anpc8323.day2;
+
+public class SimpleIfDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+		
+		int number = 15;
+		
+		if(number <= 10)
+		{
+			System.out.println(number + " is lesser then or equal to 10");
+		}
+		
+		System.out.println("End");
+
+	}
+
+}
